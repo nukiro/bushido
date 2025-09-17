f8b52e9a561d1d072995ed79acbccd15f9aac16c
Carlos Aguilar Miguel <nukiro@yahoo.com>
2025-09-17 18:30:51 +0200

    Add test option to the make script

--- make.sh
+++ make.sh
@@ -1,6 +1,39 @@
 #! bin/bash
 set -e
 
+COMMAND=$1
+
+STD="-std=gnu23"
+CFLAGS="-Wall -Wextra -Werror -g -O0"
+DDEBUG=-DDEBUG
+DTEST=-DTEST
+LRAYLIB="-lraylib -lm -ldl -lpthread -lGL -lrt -lX11"
+
+SOURCE=bushido/src
+TESTS=bushido/tests
+
+case "$COMMAND" in
+  build)
+    echo "Running build..."
+    echo ""
+    gcc $STD $DDEBUG -I$SOURCE bushido/main.c -o dist/main $LRAYLIB
+    ./dist/main
+    ;;
+  test)
+    echo "Running tests..."
+    echo ""
+    gcc $STD $CFLAGS $DTEST $DDEBUG -I$SOURCE -I$TESTS $SOURCE/*.c $TESTS/runner.c -o dist/tests $LRAYLIB
+    ./dist/tests
+    ;;
+  clean)
+    echo "Cleaning..."
+    ;;
+  *)
+    echo "Unknown command: $COMMAND"
+    exit 1
+    ;;
+esac
+
 # gcc -std=gnu23 -Wall -Wextra -Werror -g -O0 bushido/main.c -o main -lraylib -lm -ldl -lpthread -lGL -lrt -lX11 -DDEBUG
-gcc -std=gnu23 -g -O0 bushido/main.c -o main -lraylib -lm -ldl -lpthread -lGL -lrt -lX11 -DDEBUG
-./main
+# gcc -std=gnu23 -g -O0 -Ibushido/src bushido/main.c -o main -lraylib -lm -ldl -lpthread -lGL -lrt -lX11 -DDEBUG
+# ./main
