ad8c1c7a699333099f2362c92ea4cc830db1a0e7
Carlos Aguilar Miguel <nukiro@yahoo.com>
2025-09-19 12:45:21 +0200

    Improve window debug message

--- bushido/src/window.c
+++ bushido/src/window.c
@@ -5,7 +5,7 @@ void window_init(const Game *g, Manager *m)
     InitWindow(m->window.width, m->window.height, g->title);
     SetWindowTitle(m->window.title);
     SetTargetFPS(GAME_FPS);
-    DBG("window %s open: %dx%d", g->title, m->window.width, m->window.height);
+    DBG("window '%s' titled as %s, open: %dx%d", g->title, m->window.title, m->window.width, m->window.height);
     DBG("game fps: %d", GAME_FPS);
 }
 
