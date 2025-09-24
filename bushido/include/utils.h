6ae0789ea3d2def8b90f39f73d426b2ec2b0a6f8
Carlos Aguilar Miguel <nukiro@yahoo.com>
2025-09-24 08:12:04 +0200

    Utility method to check if a method run ok

---
+++ bushido/include/utils.h
@@ -0,0 +1,5 @@
+#include <string.h>
+
+// Check if a return status from a function execution returned an status error or not
+bool was_executed_successfully(int status);
+bool is_array_empty(const char *arr, size_t size);
